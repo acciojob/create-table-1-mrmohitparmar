553d1185b1f8eb6571434cd3ed984e490fae002a
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-11-13 12:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    // Table ko select karo
+	
+    let tb = document.getElementById("sampleTable");
+
+    // Nayi row banao
+    let tr = document.createElement("tr");
+
+    // Do cells banao
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+
+    // Unme text daalo
+    td1.textContent = "New Cell1";
+    td2.textContent = "New Cell2";
+
+    // Row me cells daalo
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    // Row ko table ke top me add karo (prepend)
+    tb.prepend(tr);
 }
